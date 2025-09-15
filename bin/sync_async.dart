e29c64d85692ddcb62bf6f301eba9ae7b9c1e7ca
Patrícia Belli <paty21belli@gmail.com>
2025-09-15 20:12:59 -0300

    Geradores Assíncronos

--- bin/sync_async.dart
+++ bin/sync_async.dart
@@ -1,10 +1,14 @@
 void main() {
-
-  var future = Future.delayed(Duration(seconds: 2));
-  future.then((value) => print('4'),);
-
+  print4();
   print('1');
   print('2');
   print('3');
-
 }
+
+Future<void> print4() async {
+
+  await Future.delayed(Duration(seconds: 2));
+
+  print('4');
+}
+
--- test/dart_application_1_programa_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_application_1_programa/dart_application_1_programa.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
