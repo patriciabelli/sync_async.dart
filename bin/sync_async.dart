68e4a56bc74637263593e18585a9a0497b5d551b
Patrícia Belli <paty21belli@gmail.com>
2025-09-24 18:55:35 -0300

    Mapeando objeto

--- bin/sync_async.dart
+++ bin/sync_async.dart
@@ -3,16 +3,45 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future main() async {
-  final json = await fetch();
-  print(json['id']);
+  final todo = await fetch();
+  print(todo.completed);
 }
 
-  Future<Map> fetch() async {
+  Future<Todo> fetch() async {
     var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
     var response = await http.get(url);
     var json = jsonDecode(response.body);
-    return json;
+    var todo = Todo.fromJson(json);
+    
+    return todo;
   }
+
+class Todo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  Todo({required this.title, required this.id, required this.userId, required this.completed});
+
+  factory Todo.fromJson(Map json){
+    return Todo(
+    title: json['title'],
+    id: json['id'], 
+    userId: json['userId'], 
+    completed: json['completed'],
+    );
+  }
+
+  Map toJson() {
+    return{
+      title: title,
+    id: id,
+    userId: userId,
+    completed: completed,
+    };
+  }
+}
 
 
  
