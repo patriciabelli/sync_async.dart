6bedf58cbf08cdee369b01cdb9b698ff35666c0d
Patrícia Belli <paty21belli@gmail.com>
2025-09-24 19:46:15 -0300

    Tratamento de excessões (try catch)

--- bin/sync_async.dart
+++ bin/sync_async.dart
@@ -1,47 +1,21 @@
-import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-
-Future main() async {
-  final todo = await fetch();
-  print(todo.completed);
+void main() {
+  print(stringToInt('d'));
 }
 
-  Future<Todo> fetch() async {
-    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    var response = await http.get(url);
-    var json = jsonDecode(response.body);
-    var todo = Todo.fromJson(json);
-    
-    return todo;
+int stringToInt(String text){
+  int value;
+
+  try {
+    value = int.parse(text);  
+  } catch (e) {
+    throw Exception('O text passado ($text) não e um número.');
   }
+  
 
-class Todo {
-  final String title;
-  final int id;
-  final int userId;
-  final bool completed;
-
-  Todo({required this.title, required this.id, required this.userId, required this.completed});
-
-  factory Todo.fromJson(Map json){
-    return Todo(
-    title: json['title'],
-    id: json['id'], 
-    userId: json['userId'], 
-    completed: json['completed'],
-    );
-  }
-
-  Map toJson() {
-    return{
-      title: title,
-    id: id,
-    userId: userId,
-    completed: completed,
-    };
-  }
+  return value;
 }
+  
 
 
  
